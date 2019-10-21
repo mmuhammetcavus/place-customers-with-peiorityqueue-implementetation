26ca58d9d716750347af867dd4d338ab33e89e2c
mmuhammetcavus <ahmak12345>
2019-10-21 15:33:22 +0300

    last but not least

--- RestaurantOrganizer.cpp
+++ RestaurantOrganizer.cpp
@@ -1,9 +1,9 @@
 /*
-Student Name:
-Student Number:
+Student Name:Muhammet Çavuş
+Student Number:2018400306
 Project Number: 2
-Compile Status: [SUCCESS/FAIL]
-Running Status: [SUCCESS/FAIL]
+Compile Status: [SUCCESS/FAIL] success
+Running Status: [SUCCESS/FAIL] success
 Notes: Anything you want to say about your code that will be helpful in the grading process.
 */
 #include "RestaurantOrganizer.h"
@@ -20,18 +20,69 @@ RestaurantOrganizer::RestaurantOrganizer(const vector<int>& tableCapacityInput){
 }
 
 void RestaurantOrganizer::addNewGroup(int groupSize, ofstream& outFile){
+        if(tableCapacity[heap[0]]>=groupSize) {
+            outFile<<heap[0]<<endl;
+            tableCapacity[heap[0]]-=groupSize;
+            heapDown(0);
+        } else {
+            outFile<<-1<<endl;
+        }
     // IMPLEMENT ME!
 }
 
 void RestaurantOrganizer::heapUp(int index){
+    int temp=move(heap[index]);
+    for( ; index>0 && tableCapacity[temp] > tableCapacity[ heap[(index-1) / 2] ] ; index =(index-1)/ 2 ) {
+        heap[index]=move(heap[(index-1)/2]);
+
+        if(index==0)
+            break;
+    }
+    heap[index]=temp;
+
     // IMPLEMENT ME!
 }
 
 void RestaurantOrganizer::heapDown(int index){
+
+    int temp=move(heap[index]);
+    int child;
+    while((index+1) * 2-1 < numberOfTables) {
+        child=(index+1) * 2-1;
+        if(child<numberOfTables-1 && tableCapacity[heap[child+1]]>tableCapacity[heap[child]]) {
+            child++;
+        }
+        if(child<numberOfTables-1 && tableCapacity[heap[child+1]]==tableCapacity[heap[child]]) {
+            if(heap[child+1]<heap[child])
+                child++;
+        }
+        if(tableCapacity[heap[child]]>=tableCapacity[temp]){
+            if(tableCapacity[heap[child]]>tableCapacity[temp])
+                heap[index]=move(heap[child]);
+            else {
+                if(heap[child]<temp) {
+                    heap[index]=move(heap[child]);
+                } else
+                    break;
+            }
+        } else  {
+            break;
+        }
+        index=child;
+    }
+
+    heap[index]=temp;
     // IMPLEMENT ME!
 }
 
 void RestaurantOrganizer::printSorted(ofstream& outFile){
+    //outFile<<heap[0]<<" "<<heap[1]<<" "<<heap[2]<<endl;
+    for(int i=0; i<numberOfTables; i++) {
+        outFile<<tableCapacity[heap[0]]<<" ";
+        tableCapacity[heap[0]]=-1;
+        heapDown(0);
+    }
+
     // IMPLEMENT ME!
 }
 
